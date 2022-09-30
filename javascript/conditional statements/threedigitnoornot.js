e54dd7f8678aeda68c9c9539cd1375308df62a94
sathvikapuvvada <sathvikapuvvada12@gmail.com>
2022-09-30 22:55:00 +0530

    hello

---
+++ javascript/conditional statements/threedigitnoornot.js
@@ -0,0 +1,7 @@
+let number = 45;
+if (number >= 100 & number < 1000) {
+    console.log("the given num is a 3 digit number");
+}
+else {
+    console.log("the given number is not a 3 digit number");
+}
