4074f3e0dad9eb6272c20878b46703197585eb2c
sathvikapuvvada <sathvikapuvvada12@gmail.com>
2022-09-30 22:20:42 +0530

    hello

---
+++ javascript/conditional statements/divisibilitybyseven.js
@@ -0,0 +1,7 @@
+let number = 49;
+if (number % 7 === 0) {
+    console.log("the given num is divisible by 7");
+}
+else {
+    console.log("the given number is not divisible by 7");
+}
---
+++ javascript/conditional statements/positiveornot.js
@@ -0,0 +1,7 @@
+let number = 7;
+if (number > 0) {
+    console.log("The number is positive");
+}
+else {
+     console.log("The number is negative");
+}
